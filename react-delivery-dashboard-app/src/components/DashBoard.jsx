eb3e7619831877ad3f6f3f5d1f4e425d35e6605f
roopy <swjeong1210@gmail.com>
2022-11-21 02:45:10 +0900

    Logic change

--- react-delivery-dashboard-app/src/components/DashBoard.jsx
+++ react-delivery-dashboard-app/src/components/DashBoard.jsx
@@ -14,7 +14,6 @@ const stompClient = Stomp.over(sockJS);
 
 const DashBoard = (props) => {
     const [ deliveryList, setDeliveryList ] = useState(props.items);
-    console.log(deliveryList);
 
     useEffect(() => {
         connect();
